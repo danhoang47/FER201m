d71bb8d73fd81294c10523175cc7736946e3ee8a
quocDat040702 <dathoang.040702@gmail.com>
2022-10-17 11:21:19 +0700

    Lab 6

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -1,12 +1,13 @@
 import { Component } from "react";
 import { DISHES } from "../shared/dishes";
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import Menu from "./MenuComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
-import DishDetail from './DishdetailComponent'
+import DishDetail from "./DishdetailComponent";
+import About from "./AboutComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
@@ -19,7 +20,7 @@ class Main extends Component {
             selectedDish: null,
             comments: COMMENTS,
             promotions: PROMOTIONS,
-            leaders: LEADERS
+            leaders: LEADERS,
         };
     }
 
@@ -29,19 +30,39 @@ class Main extends Component {
 
     render() {
         const HomePage = () => {
-            return(
-                <Home 
+            return (
+                <Home
                     dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                    promotion={
+                        this.state.promotions.filter(
+                            (promo) => promo.featured
+                        )[0]
+                    }
+                    leader={
+                        this.state.leaders.filter(
+                            (leader) => leader.featured
+                        )[0]
+                    }
                 />
             );
-        } 
+        };
 
-        const DishWithId = ({match}) => {
-            return(
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-                  comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        const DishWithId = ({ match }) => {
+            const numberFormat = 10;
+
+            return (
+                <DishDetail
+                    dish={
+                        this.state.dishes.filter(
+                            (dish) =>
+                                dish.id === parseInt(match.params.dishId, numberFormat)
+                        )[0]
+                    }
+                    comments={this.state.comments.filter(
+                        (comment) =>
+                            comment.dishId === parseInt(match.params.dishId, numberFormat)
+                    )}
+                />
             );
         };
 
@@ -55,8 +76,9 @@ class Main extends Component {
                         path="/menu"
                         component={() => <Menu dishes={this.state.dishes} />}
                     />
-                    <Route exact path='/contactus' component={Contact} />
-                    <Route path='/menu/:dishId' component={DishWithId} />
+                    <Route exact path="/contactus" component={Contact} />
+                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
